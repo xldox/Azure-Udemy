544529da03ec88b931f797064dbbde80c11d9788
= <leandro.lf.frazao@hotmail.com>
2024-02-19 16:13:25 -0300

    Query de teste para parametro do SSIS

---
+++ Teste parametro ssis.sql
@@ -0,0 +1,18 @@
+DECLARE @DATA_ATUAL DATE, @DATA_ATUAL_EXTERNA DATE
+
+SET @DATA_ATUAL = CAST(GETDATE() AS DATE)
+SET @DATA_ATUAL_EXTERNA = NULL -- Inicialize a variável externa conforme necessário
+
+IF @DATA_ATUAL_EXTERNA IS NULL
+	BEGIN 
+		INSERT INTO TESTE.dbo.teste_variavel
+		SELECT @DATA_ATUAL
+	END
+ELSE 
+	BEGIN
+		INSERT INTO TESTE.dbo.teste_variavel
+		SELECT @DATA_ATUAL_EXTERNA
+	END
+
+--select * from TESTE.dbo.teste_variavel
+--TRUNCATE TABLE TESTE.dbo.teste_variavel
